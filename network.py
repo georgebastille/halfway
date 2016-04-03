58d0f8d028c7b63f1b889d76b906bea8434dc624
Richie Hanes <georgebastille@hotmail.com>
2016-04-03 19:06:23 +0000

    Improved ability to detect own ip address

--- network.py
+++ network.py
@@ -1,25 +1,31 @@
 import socket
+import fcntl
+import struct
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
 
 # Returns this machines ip on it's local lan
-
-
 def mylanip():
-    return socket.gethostbyname(socket.gethostname())
+    return get_ip_address('eth0')
 
 # Returns this machines public ip address
 # (may be the address of yout router)
 # Currently not working, maybe try something with tracert
 # and find the last hop with an internal address
 # http://stackoverflow.com/a/613600
-
-
 def mywanip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("gmail.com", 80))
     addr = s.getsockname()[0]
     s.close()
     return addr
-
 
 if __name__ == '__main__':
     print "Local IP :", mylanip()
