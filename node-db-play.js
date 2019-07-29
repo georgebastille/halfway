76fe89c21e4045a74e115a52fcaf732e690417e2
Rich Hanes <georgebastille@gmail.com>
2019-07-29 06:45:28 +0100

    Need to accumulate

--- node-db-play.js
+++ node-db-play.js
@@ -1,40 +1,6 @@
 console.log("Hello there World!");
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database(
-    './halfway.db', 
-    sqlite3.OPEN_READONLY, 
-    (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to HalfwayDB');
-});
-
-const from = 'ANG';
-
-db.serialize(() => {
-  db.all(`SELECT stationb, weight FROM fullroutes 
-          WHERE stationa = ? ORDER BY stationb LIMIT 10`, [from],
-    (err, rows) => {
-      if (err) {
-        console.err(err.message);
-      }
-      console.log(rows);
-      rows.forEach((value) => {
-        console.log('Name: ' + value.STATIONB);
-      });
-    }
-  );  
-});
-
-db.close((err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Closed HalfwayDB');
-});
-
 // https://stackoverflow.com/questions/19127650/defaultdict-equivalent-in-javascript
 class DefaultDict {
   constructor(defaultInit) {
@@ -47,3 +13,43 @@ class DefaultDict {
     })
   }
 }
+
+const db = new sqlite3.Database(
+    './halfway.db', 
+    sqlite3.OPEN_READONLY, 
+    (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to HalfwayDB');
+});
+
+const from = 'ANG';
+const destinations = new DefaultDict(Array);
+
+db.serialize(() => {
+  db.all(`SELECT stationb, weight FROM fullroutes 
+          WHERE stationa = ? ORDER BY stationb LIMIT 10`, [from],
+    (err, rows) => {
+      if (err) {
+        console.err(err.message);
+      }
+      //console.log(rows);
+      rows.forEach((value) => {
+        //console.log('Name: ' + value.STATIONB);
+        destinations[value.STATIONB].push(value.WEIGHT);
+      });
+      for (let destination in destinations) {
+        console.log(destination + ': ' + destinations[destination]);
+      }
+    }
+  );  
+});
+
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Closed HalfwayDB');
+});
+
